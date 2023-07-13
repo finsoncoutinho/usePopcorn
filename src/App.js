d6775f2ba1e9621adf1c58033d6aa063c9301a84
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2023-07-13 23:34:23 +0530

    search input focus on initial render and Enter keydown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import StarRating from './StarRating';
 
@@ -164,6 +164,37 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === 'Enter') {
+          inputEl.current.focus();
+          setQuery('');
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+
+      return function () {
+        document.removeEventListener('keydown', callback);
+      };
+    },
+    [setQuery]
+  );
+
+  useEffect(function () {
+    inputEl.current.focus();
+  }, []);
+
+  // useEffect(function () {
+  //   const el = document.querySelector('.search');
+  //   el.focus();
+  // },[]);
+
   return (
     <input
       className='search'
@@ -171,6 +202,7 @@ function Search({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
